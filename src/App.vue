6e86d9dd6df3c63e05fd3d60438996fe98baa5be
Antony-Juicy <gabbiyabbiy9@gmail.com>
2021-09-14 22:00:15 +0800

    配置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,172 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{count}}</div>
+    <!-- <div>{{state.count}}</div> -->
+     <div>{{count}},{{double}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {reactive,ref, toRefs,toRef, computed,} from '@vue/composition-api';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){ // vue 3.0 的入口函数，beforeCreate之前进行触发
+
+   //具备数据响应式的
+  // const state=reactive({
+  //   count:0
+  // });
+
+  // setTimeout(()=>{
+  //   state.count++;
+  // },1000);
+
+  //  return state;
+
+  //  具备数据响应式的
+  // const state=reactive({
+  //   count:0
+  // });
+
+  // setTimeout(()=>{
+  //   state.count++;
+  // },1000);
+
+  //  return {
+  //    state
+  //    };
+
+
+   //具备数据响应式的
+//  const count=ref(0);
+   
+//         setTimeout(()=>{
+//     count.value++;
+//     },1000);
+
+//   return {
+//   count
+//   }; 
+
+  //不具备数据响应式的
+  // const state=reactive({
+  //   count:0
+  // });
+
+  //     setTimeout(()=>{
+  //     state.count++;
+  //   },1000);
+
+  //  return {
+  //    count:state.count
+  //    }; 
+
+  
+  // const count=ref(2);
+  // const state=reactive({
+  //   count
+  // });
+
+  //    setTimeout(()=>{
+  //   state.count++;
+  // },1000);
+
+  //   return {
+  //    count
+  //    }
+
+ 
+
+
+
+  // const state=reactive({
+  //   count:0
+  // });
+
+  // const {count}=toRefs(state); // 就是把普通的数据，转成ref()方法所对应的响应式数据
+   
+  //  setTimeout(()=>{
+  //   state.count++;
+  // },1000);
+
+  // return{
+  //   count 
+  // }
+
+
+// const state=reactive({
+//     count:0
+//   });
+
+// // toRefs(state) 相当于解构
+//   const {count}=toRefs(state); // 就是把普通的数据，转成ref()方法所对应的响应式数据
+    
+//    setTimeout(()=>{
+//     state.count++;
+//   },1000);
+
+//   return toRefs(state) 
+
+
+//  const state=reactive({
+//     count:0
+//   });
+
+//   const count=toRef(state,'count'); // 就是把需要的数据，转成ref()方法所对应的响应式数据
+   
+//    setTimeout(()=>{
+//     state.count++;
+//   },1000);
+
+//   return{
+//     count 
+//   }
+
+ 
+//  计算属性 computed
+//  const count=ref(1);
+//  const double=computed(()=>{
+//    return count.value*2
+//  })
+
+//    setTimeout(()=>{
+//     count.value++;
+//   },1000);
+
+
+//   return{
+//     count,
+//     double
+//   }
+
+
+    const state=reactive({
+      count:2,
+      double:computed(()=>{
+       return state.count*2
+       })
+    });
+
+    return state
+    // return toRefs(state) 
+
+
+
   }
 }
 </script>
-
+ 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  -webkit-font-smoothing: antialiased; 
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 50px  ;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ComPositonApi from '@vue/composition-api'
+Vue.use(ComPositonApi)
+
+
 Vue.config.productionTip = false
 
 new Vue({
